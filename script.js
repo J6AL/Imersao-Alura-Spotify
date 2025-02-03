df92f8abdb2eb2bed760d0d0488c03c456ca0887
André Linhares <126906729+J6AL@users.noreply.github.com>
2025-02-03 17:11:45 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
+});
+
+
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+const greetingMessage =
+currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
+
+const container = document.querySelector(".offer_list-item");
+
+const observer = new ResizeObserver(() => {   
+  const containerWidth = container.offsetWidth; 
+  const numColumns = Math.floor(containerWidth / 200); 
+
+container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+
+console.log({ container });
+console.log({ numColumns });
+});
+
+observer.observe(container);
